df42ab46f0a99d6fe67e60ac769063c8f74657cb
Luke Thomas <lukethethomas@gmail.com>
2016-10-19 21:03:26 -0700

    made code nicer

--- wordgen.py
+++ wordgen.py
@@ -1,52 +1,26 @@
 #!/usr/bin/env python3
 
+# Author: Luke Thomas
+# Date: 10/19/16
+# Description:
+
 import sys
+import wordgenmethods as m
 
 # Create file called 'words.txt' where this file is located
 txtFile = open('./words.txt', 'w');
 
-# Loop until valid input
-while(True):
-    # Get the lower bound of the length of the strings
-    minLength = int(input("Minimum length of words: "))
-    # Get the upper bound of the length of the strings
-    maxLength = int(input("Maximum length of words: "))
-
-    # If any of the input is invalid, loop again
-    if(minLength < 1 or minLength > maxLength or maxLength > 20):
-        print("Invalid input, try again.")
-    else:
-        break
+min_max = m.get_min_max();
+minLength = min_max[0];
+maxLength = min_max[1];
 
 # For strings of lengths minLength to maxLength
 for i in range(minLength, maxLength + 1):
     print("Generating strings of length", i)
-
-    # Initialize an array of size i filled with 'a' characters
-    word = ['a'] * i
-
-    # Write all strings of length i to the file
-    while(True):
-        # Cast the list to a string and write it to the file
-        #txtFile.write(''.join(word))
-        txtFile.write(''.join(word) + '\n')
-
-        # Increment the rightmost character
-        word[i - 1] = chr(ord(word[i - 1]) + 1)
-
-        # Handle the changing of the characters sequentially
-        for j in range(1, i):
-            if(ord(word[i - j]) == (ord('z') + 1)):
-                word[i - j] = 'a'
-                word[i - j - 1] = chr(ord(word[i - j - 1]) + 1)
-            else:
-                break
-
-        # If the last character got bumped past 'z', go to next length
-        if(word[0] == chr(ord('z') + 1)):
-            break
+    m.write_strings(i, txtFile);
 
 # Close the opened file
 txtFile.close()
 
 print("Finished generating words.")
+input("Press any key to exit.")
---
+++ wordgenmethods.py
@@ -0,0 +1,46 @@
+# Author: Luke Thomas
+# Date: 10/19/16
+# Description: Methods used in wordgen.py
+
+def get_min_max():
+    # Loop until valid input
+    while(True):
+        # Get the lower bound of the length of the strings
+        minLength = int(input("Minimum length of words: "))
+        # Get the upper bound of the length of the strings
+        maxLength = int(input("Maximum length of words: "))
+
+        # If any of the input is invalid, loop again
+        if(minLength < 1 or minLength > maxLength or maxLength > 20):
+            print("Invalid input, try again.")
+        else:
+            return [minLength, maxLength]
+
+
+# Write all strings of length given to the text file indicated
+def write_strings(length, txtFile):
+    # Initialize an array of size i filled with 'a' characters
+    word = ['a'] * length
+
+    # Write all strings of length given to the file
+    while(True):
+        # Cast the list to a string and write it to the file
+        txtFile.write(''.join(word) + '\n')
+
+        # Increment the rightmost character
+        word[length - 1] = chr(ord(word[length - 1]) + 1)
+        handle_overflow(word, length);
+
+        # If the last character got bumped past 'z', go to next length
+        if(word[0] == chr(ord('z') + 1)):
+            break
+
+
+# If a letter is past 'z', set it to 'a' and increment previous letter
+def handle_overflow(word, length):
+    for index in range(1, length):
+        if(ord(word[length - index]) == (ord('z') + 1)):
+            word[length - index] = 'a'
+            word[length - index - 1] = chr(ord(word[length - index - 1]) + 1)
+        else:
+            return word
